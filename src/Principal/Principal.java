2ba0efe86f2367c09641a6bcf01f6cc73a9ef307
Douglas <soporte.jd.soluciones@gmail.com>
2023-03-20 19:47:56 -0600

    Modificaciones en Clase principal

--- src/Principal/Principal.java
+++ src/Principal/Principal.java
@@ -1,10 +1,11 @@
 
 package Principal;
+
 import gui.FrmMenuPrincipal;
+
 public class Principal {
-public static void main(String[] args) {
-FrmMenuPrincipal conversionMonedaFrame = new FrmMenuPrincipal();
-	conversionMonedaFrame.setVisible(true);
-	 
-}
+	public static void main(String[] args) {
+		FrmMenuPrincipal conversionMonedaFrame = new FrmMenuPrincipal();
+		conversionMonedaFrame.setVisible(true);
+	}
 }
